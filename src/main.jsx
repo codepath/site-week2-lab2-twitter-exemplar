a18dddeca5bc0ab91b915d26ddab2570b95c83cf
Yilika Loufoua <yilika.loufoua@tcu.edu>
2023-03-01 00:43:24 -0600

    Update React and remove TDD

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { InstantNoodles } from "instant-noodles"
-import App from "./App"
-import "./globals.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./globals.css";
 
-import { testPropDrilling } from "./tests/feature-001-prop-drilling.test"
-import { testComplexStateHandling } from "./tests/feature-002-complex-state-updates.test"
-import { testControlledInputs } from "./tests/feature-003-creating-controlled-inputs.test"
-import { testReactiveStateUpdates } from "./tests/feature-004-reactive-state-updates.test"
-
-const tests = {
-  propDrilling: testPropDrilling,
-  complexState: testComplexStateHandling,
-  controlledInputs: testControlledInputs,
-  reactiveStateUpdates: testReactiveStateUpdates,
-}
-
-export const config = {}
+export const config = {};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* Leave this here for live test environment */}
-    <InstantNoodles RootComponent={App} tests={tests} config={config} />
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
